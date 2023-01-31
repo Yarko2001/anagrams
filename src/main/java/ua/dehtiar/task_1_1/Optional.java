356fa39c6345e0ff9e5ce4f464999341057a3158
Degtiar Yaroslav <merrynan2001@gmail.com>
2023-01-31 19:33:32 +0300

    created a main method

---
+++ src/main/java/ua/dehtiar/task_1_1/Optional.java
@@ -0,0 +1,16 @@
+package ua.dehtiar.task_1_1;
+
+
+/**
+ * @author Yaroslav Dehtiar on 30.01.2023
+ */
+public class Optional {
+
+
+  public static void main(String[] args) {
+    Application app = new Application();
+    System.out.println(app.reverseOnlyOneWord("hello"));
+    System.out.println(app.reverseAndSplitTheText("a1bcd efg!h"));
+  }
+
+}
